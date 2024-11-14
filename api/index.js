c52f922f0e0d4822a693c97deedb01730f0b3d7d
lucifer1017 <lucifergrey1017@gmail.com>
2024-11-14 20:45:36 +0530

    Dev commit

---
+++ api/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+dotenv.config();
+const jwt = require('jsonwebtoken');
+
+mongoose.connect(process.env.MONGO_URL);
+const app = express();
+app.use(express.json());
+const User = require('./models/User')
+const cors = require('cors');
+const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}))
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await User.findOne({ username });
+    if (foundUser) {
+        const passOk = bcrypt.compareSync(password, foundUser.password);
+        if (passOk) {
+            jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token, { sameSite: 'none', secure: true }).json({
+                    id: foundUser?._id,
+                })
+            })
+        }
+    }
+})
+app.get('/profile', (req, res) => {
+    const token = req.cookies?.token;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+            if (err) throw err;
+            res.json(userData)
+        })
+    }
+    else {
+        res.status(401).json('no token');
+    }
+
+})
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+        const createdUser = await User.create({
+            username: username
+            , password: hashedPassword
+        });
+        jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+
+            res.cookie('token', token, { sameSite: 'none', secure: true }).status(201).json({
+                id: createdUser._id,
+                username,
+            });
+        })
+    } catch (error) {
+        if (error)
+            throw error;
+        res.status(500).json('error');
+
+    }
+})
+
+app.listen(8000);
